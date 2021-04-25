fb0074e8ba7e5410848c1d03c76a4e3bea426291
Ty Painter <ty.d.painter@vanderbilt.edu>
2021-04-25 13:53:42 -0500

    fixed searchCrime.sql

--- FrontEnd/searchCrime.sql
+++ FrontEnd/searchCrime.sql
@@ -6,12 +6,8 @@ BEGIN
     SELECT complaint_info.complaint_num, suspect.suspect_sex, suspect.suspect_age_group, suspect.suspect_race,
 			offense.offense_desc
 	FROM complaint_info complaint_info
-	JOIN suspect suspect on suspect.complaint_num = complaint_info.complaint_num
-	JOIN offense offense on offense.ky_code = complaint_info.ky_code
+	LEFT JOIN suspect suspect on complaint_info.complaint_num = suspect.complaint_num
+	LEFT JOIN offense offense on complaint_info.ky_code = offense.ky_code 
 	WHERE complaint_info.complaint_num = comp_number;
 END //
 DELIMITER ;
-
-call searchCrime(492142357);
-
-select * from complaint_info;
