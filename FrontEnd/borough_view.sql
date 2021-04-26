fac17fe87d66e9a4b12b6b6686a6a340d9c9d96d
Ty Painter <ty.d.painter@vanderbilt.edu>
2021-04-25 19:50:10 -0500

    finalized repo

--- FrontEnd/borough_view.sql
+++
@@ -1,8 +0,0 @@
-DROP VIEW IF EXISTS borough_view;
-CREATE VIEW  borough_view AS # view to see the number of crimes committed in each borough in descending order
-SELECT borough_name, COUNT(*) as crime_count
-FROM crime_info
-GROUP BY borough_name
-ORDER BY COUNT(*)  DESC;
-
-select * from borough_view;
--- FrontEnd/category_view.sql
+++
@@ -1,8 +0,0 @@
-USE nypd_police;
-DROP VIEW IF EXISTS category_view;
-CREATE VIEW  category_view AS # view to see the type of crimes committed 
-SELECT law_category_code AS crime_type, COUNT(*) as crime_count
-FROM crime_info
-GROUP BY law_category_code
-ORDER BY COUNT(*) DESC;
-
